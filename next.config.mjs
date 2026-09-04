2354b8ceed48e62df682e493216a6ae2498b574a
zish4u <zish4u@gmail.com>
2026-09-04 19:47:46 +0530

    image issue fixed and nextjs, react and axios version updated.

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: '*.supabase.co',
             },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
         ],
     },
     async redirects() {
